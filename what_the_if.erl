b2e1de8907188b6fe5509aebe38b6210da326ba3
Xavier Briand <xavierbriand@gmail.com>
2013-12-20 16:51:45 -0500

    added what_the_if:oh_god/1

--- what_the_if.erl
+++ what_the_if.erl
@@ -1,5 +1,5 @@
 -module(what_the_if).
--export([heh_fine/0]).
+-export([heh_fine/0, oh_god/1]).
 
 heh_fine() ->
     if 1 =:= 1 ->
@@ -10,4 +10,9 @@ heh_fine() ->
     end,
     if 1 =:= 2, 1 =:= 1 ->
         fails
+    end.
+
+oh_god(N) ->
+    if N =:= 2 -> might_succeed;
+        true -> always_does %% this is Erlang's if's 'else!'
     end.
