fe1566b29eb5d6ab3bb7d31c5cd5edb1f729f3a1
Xavier Briand <xavierbriand@gmail.com>
2013-12-20 16:18:26 -0500

    added functions:valid_date/1)

--- functions.erl
+++ functions.erl
@@ -7,3 +7,9 @@ second([_|[X|_]]) -> X.
 
 same(X,X) -> true;
 same(_,_) -> false.
+
+valid_time({Date = {Y,M,D}, Time = {H,I,S}}) ->
+    io:format("The Date Tuple (~p) says today is: ~p/~p/~p,~n", [Date,Y,M,D]),
+    io:format("The Time Tuple (~p) indicates: ~p:~p:~p,~n", [Time,H,I,S]);
+valid_time(_) ->
+    io:format("Stop feeding me wrong data!~n").
