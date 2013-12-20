c802e91e8b9d082ace3861174aaa23bace7246e3
Xavier Briand <xavierbriand@gmail.com>
2013-12-20 16:54:31 -0500

    added what_the_if:help_me/1

--- what_the_if.erl
+++ what_the_if.erl
@@ -1,5 +1,5 @@
 -module(what_the_if).
--export([heh_fine/0, oh_god/1]).
+-export([heh_fine/0, oh_god/1, help_me/1]).
 
 heh_fine() ->
     if 1 =:= 1 ->
@@ -16,3 +16,14 @@ oh_god(N) ->
     if N =:= 2 -> might_succeed;
         true -> always_does %% this is Erlang's if's 'else!'
     end.
+
+%% Note that this one would be better as a pattern match in function heads!
+%% I'm doing it this way for the sake of the example.
+help_me(Animal) ->
+    Talk = if Animal == cat -> "meow";
+              Animal == beef -> "mooo";
+              Animal == dog -> "bark";
+              Animal == tree -> "bark";
+              true -> "fgdadfgna"
+    end,
+    {Animal, "says " ++ Talk ++ "!"}.
