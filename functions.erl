603fd3fb3026ad4c1c8ba9e6c58a17cc802b97a8
Xavier Briand <xavierbriand@gmail.com>
2013-12-20 16:41:17 -0500

    added functions with guards in "functions" module

--- functions.erl
+++ functions.erl
@@ -13,3 +13,12 @@ valid_time({Date = {Y,M,D}, Time = {H,I,S}}) ->
     io:format("The Time Tuple (~p) indicates: ~p:~p:~p,~n", [Time,H,I,S]);
 valid_time(_) ->
     io:format("Stop feeding me wrong data!~n").
+
+old_enough(Age) when Age >= 18 -> true;
+old_enough(_) -> false.
+
+right_age(Age) when Age >= 16, Age =< 77 -> true;
+right_age(_) -> false.
+
+right_age2(Age) when Age < 16; Age > 77 -> false;
+right_age2(_) -> true.
