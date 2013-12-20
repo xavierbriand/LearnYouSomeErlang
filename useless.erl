16cefdb5673dfbba9c02054d33af852a71218f45
Xavier Briand <xavierbriand@gmail.com>
2013-12-20 15:59:05 -0500

    added macro to useless module

--- useless.erl
+++ useless.erl
@@ -1,5 +1,6 @@
 -module(useless).
--export([add/2, hello/0, great_and_add_two/1])
+-export([add/2, hello/0, great_and_add_two/1]).
+-define(HELLO, "Hello, world!~n").
 
 add(A,B) ->
     A + B.
@@ -7,7 +8,7 @@ add(A,B) ->
 %% show greetings
 %% io:format/1 is the standard function used to output text.
 hello() ->
-    io:format("Hello, world!~n").
+    io:format(?HELLO).
 
 great_and_add_two(X) ->
     hello(),
