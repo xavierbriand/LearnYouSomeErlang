11c03fb224a1982e22686eb50e09185c70e164c3
Xavier Briand <xavierbriand@gmail.com>
2013-12-30 07:15:33 -0500

    added recursive:tail_fac/1

--- recursive.erl
+++ recursive.erl
@@ -1,8 +1,12 @@
 -module(recursive).
--export([fac/1,len/1]).
+-export([fac/1,len/1,tail_fac/1]).
 
 fac(0) -> 1;
 fac(N) when N > 0 -> N*fac(N-1).
 
 len([]) -> 0;
 len([_|T]) -> 1 + len(T).
+
+tail_fac(N) -> tail_fac(N, 1).
+tail_fac(0,Acc) -> Acc;
+tail_fac(N,Acc) when N > 0 -> tail_fac(N-1, Acc*N).
